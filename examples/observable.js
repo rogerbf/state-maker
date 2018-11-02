63eb1574286641e0eaf55336f2e9765bb2adad3c
rogerbf <roger@bergfa.lk>
2018-11-02 22:13:31 +0100

    add example using makeObservable

---
+++ examples/observable.js
@@ -0,0 +1,15 @@
+const { createState, makeObservable } = require(`../`)
+const { from } = require(`rxjs`)
+
+const state = createState(makeObservable)
+
+const subscription = from(state).subscribe(console.log)
+
+state(`beep`)
+state(`boop`)
+
+subscription.unsubscribe()
+
+state(`goodbye`)
+
+console.log(`state:`, state.current)
